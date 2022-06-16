aacae03eb6e0b4f3b1cfb44e0f9ad4c96bcd6a09
Jason <jasonliu215@gmail.com>
2022-06-17 05:16:25 +1200

    Forms broken but it resets after submission

--- frontend/src/components/pages/images/AddImage.js
+++ frontend/src/components/pages/images/AddImage.js
@@ -1,12 +1,14 @@
 import classes from './AddImage.module.css'
-import {Formik, Form, Field, ErrorMessage, useFormik} from 'formik';
+import {Formik, Form, Field} from 'formik';
 import * as Yup from "yup"
-import {Button, LinearProgress, Box} from '@mui/material';
+import {Button, Box} from '@mui/material';
 import {TextField} from 'formik-mui';
 import axios from 'axios';
+import {useState, useEffect} from "react";
 
 
 function AddImage() {
+    const [tagOptions, setTagOptions] = useState([])
     const initialValues = {
         name: '',
         description: '',
@@ -23,20 +25,43 @@ function AddImage() {
     })
 
 
-    const onSubmit = (values) => {
+    const onSubmit = (values, {resetForm}) => {
         let form_data = new FormData();
         form_data.append('name', values.name);
         form_data.append('description', values.description);
         form_data.append('image', values.file, values.file.name);
+        form_data.append('tag', values.tag);
         let url = 'http://localhost:8000/api/images/';
         axios.post(url, form_data, {
             headers: {
                 'content-type': 'multipart/form-data'
             }
         })
+            .then(() => resetForm({values: ''}))
             .catch(err => console.log(err))
     };
 
+    const createTagOptions = () => {
+        let items = [];
+        for (const tagOption of tagOptions) {
+            items.push(
+                <option
+                    key={tagOption.id}
+                    value={tagOption.name}
+                >
+                    {tagOption.name}
+                </option>)
+        }
+        return items
+    }
+
+    useEffect(() => {
+        let url = `http://localhost:8000/api/tags/`;
+        fetch(url)
+            .then(response => response.json())
+            .then(json => setTagOptions(json))
+            .then(() => createTagOptions())
+    }, [])
 
     return (
         <div>
@@ -74,6 +99,8 @@ function AddImage() {
                                         name="description"
                                         type="text"
                                         label="Description"
+                                        multiline
+                                        rows={4}
                                         component={TextField}
                                     />
                                 </Box>
@@ -88,6 +115,16 @@ function AddImage() {
                                         onChange={event => props.setFieldValue("file", event.currentTarget.files[0])}
                                     />
                                 </Box>
+
+                                <Box margin={2}>
+                                    <Field as="select"
+                                           name="tag"
+                                           id="tag"
+                                    >
+                                        {createTagOptions}
+                                    </Field>
+                                </Box>
+                                <br/>
                             </div>
                             <Button type="submit">Submit</Button>
                         </Form>
--- frontend/src/components/pages/tags/AddTag.js
+++ frontend/src/components/pages/tags/AddTag.js
@@ -1,9 +1,89 @@
 import classes from './AddTag.module.css'
+import {Formik, Form, Field} from 'formik';
+import * as Yup from "yup"
+import {Button, Box} from '@mui/material';
+import {TextField} from 'formik-mui';
+import axios from 'axios';
+import {useState, useEffect} from "react";
 
 
 function AddTag() {
+    const initialValues = {
+        name: '',
+        description: '',
+    }
+
+    const validationSchema = Yup.object({
+        name: Yup.string()
+            .max(15, "Must be 15 characters or less")
+            .required("Tag Name Required"),
+        description: Yup.string(),
+    })
+
+
+    const onSubmit = (values, {resetForm}) => {
+        let url = 'http://localhost:8000/api/tags/';
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(values)
+        })
+            .then(resetForm({values: ''}))
+            .catch(err => console.log(err))
+    };
+
     return (
-        <h1>This is the AddTag</h1>
+        <div>
+            <Formik
+                initialValues={initialValues}
+                validationSchema={validationSchema}
+                onSubmit={onSubmit}
+            >
+                {(props) => {
+                    const {
+                        values,
+                        handleChange,
+                        handleBlur,
+                        handleSubmit,
+                        errors
+                    } = props;
+                    return (
+                        <Form>
+                            <h1>Add Tags</h1>
+                            <div>
+                                <Box margin={2}>
+                                    <Field
+                                        id="name"
+                                        name="name"
+                                        type="text"
+                                        label="Name"
+                                        component={TextField}
+                                    />
+                                </Box>
+                                <br/>
+                                <Box margin={2}>
+                                    <Field
+                                        as='textarea'
+                                        id="description"
+                                        name="description"
+                                        type="text"
+                                        label="Description"
+                                        multiline
+                                        rows={4}
+                                        component={TextField}
+                                    />
+                                </Box>
+                                <br/>
+                            </div>
+                            <Button type="submit">Submit</Button>
+                        </Form>
+                    )
+                }}
+            < /Formik>
+        </div>
     );
 }
 
