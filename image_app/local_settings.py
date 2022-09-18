5c24d8f5b2a752a233c0060de97c939870d355fe
Jason Liu <jasonliu215@gmail.com>
2022-09-18 19:48:34 +1200

    Change some db settings

--- image_app/local_settings.py
+++ image_app/local_settings.py
@@ -3,8 +3,8 @@ import os
 DATABASES = {
     "default": {
         "ENGINE": os.environ.get("SQL_ENGINE", "django.db.backends.postgresql"),
-        "NAME": os.environ.get("SQL_DATABASE", "image_app"),
-        "USER": os.environ.get("SQL_USER", "image_app_user"),
+        "NAME": os.environ.get("SQL_DATABASE", "latte_db"),
+        "USER": os.environ.get("SQL_USER", "latte_user"),
         "PASSWORD": os.environ.get("SQL_PASSWORD", "postgres"),
         "HOST": os.environ.get("SQL_HOST", "localhost"),
         "PORT": os.environ.get("SQL_PORT", "5432"),
