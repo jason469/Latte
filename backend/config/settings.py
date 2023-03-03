4a5efc97f0265c994c5ef71587e598d95efdb815
Jason Liu <jasonliu215@gmail.com>
2023-03-04 12:29:49 +1300

    Updated DB

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -71,7 +71,7 @@ TEMPLATES = [
 WSGI_APPLICATION = 'backend.config.wsgi.application'
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'), conn_max_age=600),
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL')),
 }
 
 # DATABASES = {
