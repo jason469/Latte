86667fa05d45f063f9c8a1711a26a9503536d619
Jason Liu <jasonliu215@gmail.com>
2023-03-04 11:48:32 +1300

    Adding prod settings

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -10,12 +10,9 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 SECRET_KEY = os.environ.get('SECRET_KEY', "default secret key")
-
 DEBUG = os.environ.get('DEBUG', 0)
-
-ALLOWED_HOSTS = ['127.0.0.1', '0.0.0.0', 'localhost']
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(' ')
 
 INSTALLED_APPS = [
     # Django apps
